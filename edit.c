c886506c78dfd8eaa0b9e12196e8c208425bf040
bunseng <seng4510@gmail.com>
2018-03-28 18:26:08 -0500

    added polish

--- edit.c
+++ edit.c
@@ -1,28 +1,56 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "edit.h"
+
 // move the cursor up one position
 void moveup()
 {	
 	getyx(stdscr, y, x);
 	move(y - 1, x);
 }
+// move the cursor up one position in window 'win'
+void wmoveup(WINDOW * win){
+	getyx(win, y, x);
+	wmove(win, y - 1, x);
+}
 // move the cursor down one position
 void movedown(){
 	getyx(stdscr, y, x);
 	move(y + 1, x);
+}
+// move the cursor down one position in window 'win'
+void wmovedown(WINDOW * win){
+	getyx(win, y, x);
+	wmove(win, y + 1, x);
 }
 // move the cursor left one postion
 void moveleft(){
 	getyx(stdscr, y, x);
 	move(y, x - 1);
 }
+// move the cursor left one position in window 'win'
+void wmoveleft(WINDOW * win){
+	getyx(win, y, x);
+	wmove(win, y, x - 1);
+}
 // move the cursor right one position
 void moveright(){
 	getyx(stdscr, y, x);
 	move(y, x + 1);
 }
+// move the cursor right one position in window 'win'
+void wmoveright(WINDOW * win){
+	getyx(win, y, x);
+	wmove(win, y, x + 1);
+}
 // delete the current line
 void deleteline(){
 	deleteln();
+}
+// delete line in window 'win'
+void wdeleteline(WINDOW * win){
+	wdeleteln(win);
 }
 // insert a line at the current position
 void insertline(){
@@ -30,10 +58,21 @@ void insertline(){
 	insertln();
 	move(y + 1, x);
 }
+// insert a line into window 'win'
+void winsertline(WINDOW * win){
+	getyx(win, y, x);
+	winsertln(win);
+	wmove(win, y + 1, x);
+}
 // delete the character to the left of the cursor
 void backspace(){
 	getyx(stdscr, y, x);
 	mvdelch(y, x - 1);
+}
+// delete the character to the left in window 'win'
+void wbackspace(WINDOW * win){
+	getyx(win, y, x);
+	mvwdelch(win, y, x - 1);
 }
 // insert a character and shift line to the right
 void insertchar(char c){
@@ -41,23 +80,112 @@ void insertchar(char c){
 	insch(c);
 	move(y, x + 1);	
 }
+// insert a character to a specified window
+void winsertchar(WINDOW* win, char c){
+	getyx(win, y, x);
+	winsch(win, c);
+	wmove(win, y, x + 1);
+}
 // enable command mode
-void commandmodeon(){
-	getyx(stdscr, y, x);
-	move(30, 0);
-	deleteline();
-	printw("Command Mode: 'I': Enable Insert Mode 'X': Delete Line 'O': Insert Line 'S': Save File");
-	move(y, x);
+void commandmodeon(WINDOW * win){
+	getyx(win, y, x);
+	mvwprintw(stdscr, 1, 0, "Command Mode: ");
+	//mvwprintw(stdscr, 2, 0, 
+	//"\'I\': Enter Insert Mode \'X\': Delete Line \'O\': Insert Line \'S\': Save File \'C\': Copy \'P\': Paste \'R\':
+	//Search");
+	wmove(win, y, x);
 	mode = 'c';
 }
 // disable command mode
-void commandmodeoff(){
-	getyx(stdscr, y,x);
-	move(30, 0);
-	deleteline();
-	deleteline();
-	printw("Insert Mode:");
-	move(y, x);
+void commandmodeoff(WINDOW * win){
+	getyx(stdscr, y, x);
+	mvwprintw(stdscr, 1, 0, "Insert Mode: ");
+	wmove(win, y, x);
 	mode = 'i';
 }
-	
+
+void searchReplace(const char* str,const char* old, const char* new){
+    FILE *original, *temp;
+    char choice;
+    if(choice == 'R'){
+
+    	if((original = fopen(str, "r")) == NULL){
+        perror(str);
+        exit(1);
+    }
+
+    if((temp = fopen("temp.txt", "w")) == NULL){
+        perror("text");
+        exit(1);
+    }
+
+    const int BUFFER_SIZE = fsize(str);
+    char *buffer = malloc(BUFFER_SIZE);
+    char *init_loc = buffer;
+
+    int word_len = strlen(old);
+    int counter = 0;
+
+    while(fgets(buffer, BUFFER_SIZE, original)){
+        while((buffer = strstr(buffer, old))){
+            memcpy(buffer, new, word_len);
+            counter++;
+        }
+        buffer = init_loc;
+        fprintf(temp, "%s", buffer);
+    }
+    printf("'%s' found %i times\n", old, counter);
+
+    fclose(original);
+    fclose(temp);
+
+    if((original = fopen(str, "w")) == NULL){
+        perror(str);
+        exit(1);
+    }
+
+    if((temp = fopen("temp.txt", "r")) == NULL){
+        perror("text");
+        exit(1);
+    }
+
+    int c;
+    while((c = fgetc(temp)) != EOF){
+        fputc(c, original);
+    }
+}
+
+    fclose(original);
+    fclose(temp);
+    free(buffer);
+
+}	
+
+int fsize(const char *str){
+    FILE *f;
+    if((f = fopen(str, "r")) == NULL){
+        perror(str);
+        exit(1);
+    }
+    fseek(f, 0, SEEK_END);
+    int size = ftell(f);
+    fclose(f);
+    return size;
+}
+
+void write_to_file(const char *str){
+    FILE *in;
+    char choice;
+    if (choice == 'W'){
+    if((in = fopen(str, "w")) == NULL){
+        perror(str);
+        exit(1);
+    }
+    int c;
+    while((c = getchar()) != EOF){
+        fputc(c, in);
+    }
+}
+    fclose(in);
+}
+
--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "edit.h"
+#include <string.h>
+#define WORD_SIZE 24
 
 int main()
 {
@@ -12,82 +14,125 @@ int main()
 	/* mode variable is declared in "edit.h"
 	default mode is insert mode */
 	mode = 'i';
+	
+	char * fn = "Group 7 is gay";
+	printw("Group 7: %s", fn);
+	mvwprintw(stdscr, 1, 0, "Insert Mode: "); // default is Insert mode
+
+	getmaxyx(stdscr, y, x);	
+	WINDOW * win = newwin(50, 80, 3, 0); // create new window
+	refresh(); // refresh stdscr
+
+	// create default border around 'win'
+	wborder(win, ' ', ' ', '-', ' ', '-', ' ', '-', ' ');
+	wrefresh(win);
+	
 	// open a file with read and write permission
 	char read;
 	FILE* file = fopen("file.txt", "r+");
 	if (file == NULL){
-		printw("No file..\n");
+		wprintw(win, "No file..\n");
+		wrefresh(win);
 		exit(0);
 	}
+	wmove(win, 1, 0); // don't overwrite top border
 	// display the file in new ncurses window
 	read = fgetc(file);
 	while(read != EOF){
-		printw("%c", read);
+		wprintw(win, "%c", read);
 		read = fgetc(file);
 	}
 	// choice: user input
 	// y and x used to track and move cursor
 	int choice, y, x;
+	const char filename[] = "file.txt";
+    	char old[WORD_SIZE];
+    	char new[WORD_SIZE];
+    	//puts("Enter 'w' to write to the file, 'r' to replace a word");
 	while(true){
-		refresh(); // refresh screen image
+		wrefresh(win); // refresh screen image
 		choice = getch(); // take user input
-		getyx(stdscr, y, x); // find cursor position
+		getyx(win, y, x); // find cursor position
 		//if insert mode
 		if(mode == 'i')
 		switch(choice){
 			case 27:
-			commandmodeon();
+			commandmodeon(win);
 			break;
 			case KEY_LEFT:
-			moveleft();
+			wmoveleft(win);
 			break;
 			case KEY_RIGHT:
-			moveright();
+			wmoveright(win);
 			break;	
 			case KEY_UP:
-			moveup();
+			if (y > 1)
+				wmoveup(win);
 			break;
 			case KEY_DOWN:
-			movedown();
+			if (y < 48)
+				wmovedown(win);
 			break;
 			case KEY_BACKSPACE:
-				backspace();
+				wbackspace(win);
 			break;
 			case 10: // 10 = new line character
 			if(mode == 'i')
-				insertline();
+				winsertline(win);
 			break;
 			default: // if no special cases insert character
-				insertchar(choice);
+				winsertchar(win, choice);
 			break;
 		}
 		//if command mode
 		else
 		switch(choice){
 			case KEY_LEFT:
-			moveleft();
+			wmoveleft(win);
 			break;
 			case KEY_RIGHT:
-			moveright();
+			wmoveright(win);
 			break;	
 			case KEY_UP:
-			moveup();
+			if (y > 1)
+				wmoveup(win);
 			break;
 			case KEY_DOWN:
-			movedown();
+			if(y < 48)
+				wmovedown(win);
 			break;
 			case 'I': // disable command mode
-			commandmodeoff();
+			commandmodeoff(win);
 			break;
 			case 'X': // delete current line
-			deleteline();
+			wdeleteline(win);
 			break;
 			case 'O': // insert new line
-			insertline();
+			winsertline(win);
 			break;
+			case 'W': //write to file
+				//getchar();
+				write_to_file(filename);
+			break;
+			case 'R': // search and replace
+				//getchar();
+            			wprintw(win, "Find a word: ");
+            			fgets(old, sizeof(old), stdin);
+           			old[strlen(old) - 1] = '\0';
+            			wprintw(win, "Enter the new word");
+            			fgets(new, sizeof(new), stdin);
+            			new[strlen(new) - 1]  = '\0';
+
+            			if(strlen(old) != strlen(new)){
+                			printf("Error size");
+                		break;
+            			}
+            			searchReplace(filename, old, new);
+            		break;
+			
 			default: // invalid
-			mvprintw(31, 0, "Invalid Command.");
-			move(y, x);
+			mvwprintw(stdscr, 62, 0, "Invalid Command.");
+			wmove(win, y, x);
 			break;
 		}
 	}
